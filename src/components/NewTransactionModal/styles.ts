42ec7a10720e4b292239b500a98f112031913380
Mariele Ribeiro <mariele_rbr@hotmail.com>
2021-04-01 13:21:52 -0300

    Funcionamento dos botões

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -55,32 +55,41 @@ export const TransactionTypeContainer = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr;
   gap: 0.5rem;
+`;
 
-  button {
-    height: 4rem;
-    border: 1px solid #d7d7d7;
-    border-radius: 0.25rem;
+interface RadioBoxProps {
+  isActive: boolean;
+}
 
-    background: transparent;
-    display: flex;
-    align-items: center;
-    justify-content: center;
+export const RadioBox = styled.button<RadioBoxProps>`
+  height: 4rem;
+  border: 1px solid #d7d7d7;
+  border-radius: 0.25rem;
 
-    transition: border-color 0.2s;
-    &:hover {
-      border-color: ${darken(0.1, "#d7d7d7")};
-    }
+  // toda vez que eu passar uma função dentro de uma interpolação, essa função vai ser chamanda
+  //imediatamente pelo styed-componet passando todas a proriedades do meu componente
+  background: ${(props) => (props.isActive ? "#ccc" : "transparent")};
+  // observar que não foi colocado {} porque foi feito um return
+  // quando não é colocado {} por volta de uma função o conteúdo dela faz um return automatico
 
-    img {
-      width: 20px;
-      height: 20px;
-    }
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
-    span {
-      display: inline-block;
-      margin-left: 1rem;
-      font-size: 1rem;
-      color: var(--text-title);
-    }
+  transition: border-color 0.2s;
+  &:hover {
+    border-color: ${darken(0.1, "#d7d7d7")};
+  }
+
+  img {
+    width: 20px;
+    height: 20px;
+  }
+
+  span {
+    display: inline-block;
+    margin-left: 1rem;
+    font-size: 1rem;
+    color: var(--text-title);
   }
 `;
